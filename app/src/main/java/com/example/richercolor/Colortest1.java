3051e03f4be95a2e78d8fe9b3cad7031abf77afa
Byeongdo_Choi <cookie023@Naver.com>
2020-08-26 10:09:14 +0900

    테스트 케이스 작성

---
+++ app/src/main/java/com/example/richercolor/Colortest1.java
@@ -0,0 +1,14 @@
+package com.example.richercolor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Colortest1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colortest1);
+    }
+}
---
+++ app/src/main/java/com/example/richercolor/Colortest5.java
@@ -0,0 +1,14 @@
+package com.example.richercolor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Colortest5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colortest5);
+    }
+}
